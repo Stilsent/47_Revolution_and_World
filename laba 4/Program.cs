507b4ebccb88a781db9907aa7b1276c78d4c65ce
Stilsent <vaceslavkitaev50@gmail.com>
2023-11-17 09:09:30 +0300

    47 Revolution and World

--- laba 4/Program.cs
+++ laba 4/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("47 Revolution and World");
+Console.WriteLine("Небом и звездой");
