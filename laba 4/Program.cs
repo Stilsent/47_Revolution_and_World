5c8844d4407576d5f57011384cd1e48daa81297d
Stilsent <vaceslavkitaev50@gmail.com>
2023-11-17 09:13:01 +0300

    Наши сердца

--- laba 4/Program.cs
+++ laba 4/Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("47 Revolution and World");
+Console.WriteLine("Человек, который изменил всё");
 Console.WriteLine("Небом и звездой");
+
+Console.WriteLine("Love");
+Console.WriteLine("Дыхание с тобой");
+Console.WriteLine("Поколение моё");
+
+Console.WriteLine("Небом и звездой");
+Console.WriteLine("Наши сердца");
+Console.WriteLine("Во имя любви");
